f33201e557436e2fb04e8d02aa086f5c1bdf711e
hualiangqin <46637518+hualiangqin@users.noreply.github.com>
2019-04-25 12:02:56 -0700

    Update myagenda.js

--- public/myagenda.js
+++ public/myagenda.js
@@ -33,7 +33,7 @@ back to the eight blocks
 	//purpose: this function go back to the page that contains eight blocks
 	//and stop the timer
 	function goBack() {
-		console.log(1);
+		console.log("goback is clicked");
 		document.getElementById("display").innerHTML = "";
 		document.getElementById("weekday").hidden = true;
 		document.getElementById("week").hidden = false;
